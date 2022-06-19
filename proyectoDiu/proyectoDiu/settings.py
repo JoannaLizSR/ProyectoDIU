d986de71948a708211c0549bae4b3749e0b769c1
JoannaLizSR <joanna_sanchez115@ciencias.unam.mx>
2022-06-19 16:28:53 -0500

    agregar crispy_forms a Installed_Apps en settings.py

--- proyectoDiu/proyectoDiu/settings.py
+++ proyectoDiu/proyectoDiu/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    
+    'django.contrib.staticfiles',    
     'proyectoDiuApp',
+    'crispy_forms',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
