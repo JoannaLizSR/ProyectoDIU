66c401771dc91e1cf0d7e88f12fc90caaa389d42
99joseluis <jose_luis@ciencias.unam.mx>
2022-06-19 20:15:34 -0500

    Agregando pocos cambios

--- proyectoDiu/proyectoDiuApp/migrations/0001_initial.py
+++ proyectoDiu/proyectoDiuApp/migrations/0001_initial.py
@@ -17,7 +17,8 @@ class Migration(migrations.Migration):
             fields=[
                 ('email', models.CharField(max_length=50, primary_key=True, serialize=False)),
                 ('nombre', models.CharField(max_length=50)),
-                ('fechaRegistro', models.DateField(default=datetime.datetime(2022, 6, 15, 8, 49, 49, 414305)))],
+                ('fechaRegistro', models.DateField(default=datetime.datetime(2022, 6, 15, 8, 49, 49, 414305)))],),
+        migrations.CreateModel(
             name='Cita',
             fields=[
                 ('id_cita', models.AutoField(primary_key=True, serialize=False)),
